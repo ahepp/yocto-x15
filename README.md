90e2518b5ceb800b8d0db4ec307178448c628a30
Andrew Hepp <andrew.hepp@ahepp.dev>
2023-03-09 20:32:16 -0800

    don't recommend building in docker volume

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A containerized yocto project intended for use with the [BeagleBoard-X15](https:
 ## To build
 
     $ docker build . -t $X15_IMAGE
-    $ docker run -it -v $(pwd):/ws -v $X15_VOLUME:/ws/x15 $X15_IMAGE
+    $ docker run -it -v $(pwd):/ws $X15_IMAGE
     > ./scripts/fetch.sh          # Fetch layers
     > ./scripts/build.sh          # Build core-image-minimal
--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -2,15 +2,15 @@
 
 cd $(dirname $0)/..
 
-if [[ -d /ws/x15/sources ]] ; then
-    git -C /ws/x15/sources/poky pull
-    git -C /ws/x15/sources/meta-arm pull
-    git -C /ws/x15/sources/meta-ti pull
+if [[ -d /ws/sources ]] ; then
+    git -C /ws/sources/poky pull
+    git -C /ws/sources/meta-arm pull
+    git -C /ws/sources/meta-ti pull
 else 
-    mkdir -p /ws/x15/sources
-    git clone -b kirkstone git://git.yoctoproject.org/poky     /ws/x15/sources/poky
-    git clone -b kirkstone git://git.yoctoproject.org/meta-arm /ws/x15/sources/meta-arm
-    git clone -b kirkstone git://git.yoctoproject.org/meta-ti  /ws/x15/sources/meta-ti
+    mkdir -p /ws/sources
+    git clone -b kirkstone git://git.yoctoproject.org/poky     /ws/sources/poky
+    git clone -b kirkstone git://git.yoctoproject.org/meta-arm /ws/sources/meta-arm
+    git clone -b kirkstone git://git.yoctoproject.org/meta-ti  /ws/sources/meta-ti
 fi
-mkdir -p /ws/x15/build/conf
-cp conf/* /ws/x15/build/conf/
+mkdir -p /ws/build/conf
+cp conf/* /ws/build/conf/
